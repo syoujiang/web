cffd311f2e38beca0450aca799172d575f45516d
syoujiang@gmail.com <syoujiang@gmail.com>
2013-08-02 17:35:29 +0800

    add822

--- application/logs/log-2013-07-15.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-DEBUG - 2013-07-15 11:01:26 --> Config Class Initialized
-DEBUG - 2013-07-15 11:01:26 --> Hooks Class Initialized
-DEBUG - 2013-07-15 11:01:26 --> Utf8 Class Initialized
-DEBUG - 2013-07-15 11:01:26 --> UTF-8 Support Enabled
-DEBUG - 2013-07-15 11:01:26 --> URI Class Initialized
-DEBUG - 2013-07-15 11:01:26 --> Router Class Initialized
-ERROR - 2013-07-15 11:01:27 --> 404 Page Not Found --> public
